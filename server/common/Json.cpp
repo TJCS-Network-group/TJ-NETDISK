db70e89c745aff469711545fed4e81cdd7bb813f
gzy_02 <1262454489@qq.com>
2022-06-22 22:50:53 +0800

    JSON

---
+++ server/common/Json.cpp
@@ -0,0 +1,86 @@
+#include "../include/Json.h"
+
+#include <stack>
+using namespace std;
+std::map<std::string, JSON> JSON_to_map(JSON json) {
+    string raw_json = json.raw_json;
+    map<string, JSON> res;
+    string key, value;
+    stack<char> find_value_stack;
+    bool find_key = 0, find_value = 0;
+    for (size_t i = 1; i < raw_json.size(); ++i) {
+        if (find_value == true) {
+            if (find_value_stack.size() == 0 && raw_json[i] == '}') {
+                res[key] = JSON(value);
+                cout << "key: " << key << endl << "value: " << value << endl;
+                break;
+            } else if (find_value_stack.size() == 0 && raw_json[i] == ',') {
+                res[key] = JSON(value);
+                cout << "key: " << key << endl << "value: " << value << endl;
+                value.clear();
+                find_value = false;
+            } else {
+                if (raw_json[i] == '[' || raw_json[i] == '{') {
+                    find_value_stack.push(raw_json[i]);
+                } else if (raw_json[i] == '}' || raw_json[i] == ']') {
+                    find_value_stack.pop();
+                }
+                value.push_back(raw_json[i]);
+            }
+        }
+        if (raw_json[i] == '\"' && find_value == false) {
+            if (find_key == true) {
+                // key准备好了,准备找value
+                find_key = false;
+                ++i;  //跳过冒号
+                find_value = true;
+            } else {  //准备找key
+                find_key = true;
+                key.clear();
+            }
+        } else if (find_key == true) {
+            key.push_back(raw_json[i]);
+        }
+    }
+    return res;
+}
+
+std::vector<JSON> JSON_to_vector(JSON json) {
+    vector<JSON> res;
+    string raw_json = json.raw_json;
+    stack<char> find_item_stack;
+    string target_json;
+    for (size_t i = 1; i < raw_json.size(); ++i) {
+        target_json.push_back(raw_json[i]);
+        if (raw_json[i] == '[' || raw_json[i] == '{') {
+            find_item_stack.push(raw_json[i]);
+        } else if (raw_json[i] == '}' || raw_json[i] == ']') {
+            find_item_stack.pop();
+        }
+        if (raw_json[i] == ',' && find_item_stack.size() == 0) {
+            ++i;
+            res.push_back(JSON(target_json));
+            target_json.clear();
+        }
+    }
+    return res;
+}
+
+//使用参考例：
+/*
+int test() {
+    string send_json =
+        "{\n\
+\"data\": [{\"md5\":1},{\"md5\":1}], \n\
+\"message\": \"请求格式错误\", \n\
+\"statusCode\": 400, \n\
+\"success\": false\n}\
+";
+    cout << send_json << endl;
+    JSON tep(send_json);
+    std::map<string, JSON> tep_map = JSON_to_map(tep);
+    cout << tep_map["message"].as_string() << endl;
+    cout << JSON_to_map(JSON_to_vector(tep_map["data"])[0])["md5"].as_int() << endl;
+    return 0;
+}
+*/
--- server/include/Json.h
+++ server/include/Json.h
@@ -11,4 +11,28 @@ string->bool
 要实现的核心功能为前两条
 
 */
-class Json {};
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+struct JSON {
+    std::string raw_json;
+
+    JSON(std::string s = "") {
+        for (char i : s) {
+            if (i != ' ' && i != '\n' && i != '\t' && i != '\r') {
+                raw_json.push_back(i);  //去除回车空格等字符
+            }
+        }
+    }
+
+    std::string as_string() { return raw_json.substr(1, raw_json.size() - 2); };  //主要是去除双引号
+    int as_int() { return atoi(raw_json.c_str()); };
+    // float as_float();
+    bool as_bool() {
+        if (raw_json == "true")
+            return true;
+        else
+            return false;
+    };
+};
