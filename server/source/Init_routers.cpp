0c0042141397f2e1a702324f802ece7bb1ef9fa3
gzy_02 <1262454489@qq.com>
2022-06-23 17:25:35 +0800

    route

--- server/source/Init_routers.cpp
+++ server/source/Init_routers.cpp
@@ -1,13 +1,9 @@
 #include "../include/HttpServer.h"
 
 //初始化路由表
-void Routers::Init_routers() {
-    setRouter("GET", "/test", GET_test);
-    setRouter("POST", "/test", POST_test);
-    setRouter("DELETE", "/test", DELETE_test);
-    setRouter("PUT", "/test", PUT_test);
-    setRouter("POST", "/test_gzy", POST_test_gzy);
-    setRouter("POST", "/login", POST_login);
-    setRouter("GET", "/logout", GET_logout);
-    setRouter("POST", "/register", POST_register);
+void Routers::Init_routers()
+{
+    setRouter("POST", "/api/login", POST_login);
+    setRouter("GET", "/api//logout", GET_logout);
+    setRouter("POST", "/api//register", POST_register);
 }
