0452c026fefccd41805052026dd8a627889e0621
UltraQ123 <1355211477@qq.com>
2022-06-24 23:46:26 +0800

    remove_file

--- server/include/HttpTool.h
+++ server/include/HttpTool.h
@@ -6,5 +6,6 @@
 int FileToStr(std::string filepath, std::string &result);
 std::string bool_to_string(bool _success);
 std::string To_gbk(std::string utf8_str);
+bool file_exists(const std::string &name);
 int popen_cmd(std::string cmd, std::string &string, const int max_len);
 int Parse_params(const std::string &params_str, std::map<std::string, std::string> &params);
